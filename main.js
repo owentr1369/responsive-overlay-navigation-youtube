15db282f162cf66020f6dae2eaa1d75beeb2586f
TranTam <tamvaa1306@gmail.com>
2022-07-29 22:12:14 +0700

    finish video

---
+++ main.js
@@ -0,0 +1,7 @@
+function menuToggle() {
+  const nav = document.getElementById("nav");
+  const toggle = document.getElementById("toggle");
+
+  nav.classList.toggle("active");
+  toggle.classList.toggle("active");
+}
